98f6ba3d6ba11a0b4b4307864896b02621aa7e2c
Quentin Hocdé <quentinhocde@gmail.com>
2015-09-10 12:33:09 +0200

    add smooth parallax dynamic

--- js/App.js
+++ js/App.js
@@ -13,6 +13,18 @@ class App {
 		this.$container = document.getElementById("wrapper");
 		this.transform = Utils.GetSupportedPropertyName('transform');
 
+		//Array with 2 entries : top value of element, and the element
+		this.arrayMoves = new Array();
+
+		//Array of Elements wich want to have a parallax
+		this.$moves = document.getElementsByClassName("move");
+
+		//Add element to arrayMoves
+		for (var i = this.$moves.length - 1; i >= 0; i--) {
+			this.arrayMoves.push({top:0,el:this.$moves[i]});
+		}
+
+
 		//Request Animation Frame to handle scroll
 		function reqAF(){
 			this.handleScroll();
@@ -29,6 +41,17 @@ class App {
 		y = -this.containerTop;
 		this.$container.style[this.transform] = 'translate3d(0, ' + y + 'px, 0)';
 
+		//For into the arrayMoves to change the top value and keep him to have a velocity
+		for (var i = this.arrayMoves.length - 1; i >= 0; i--) {
+			let y;
+
+			this.arrayMoves[i].top += this.arrayMoves[i].el.getAttribute('data-speed') * 0.1 * (this.scrollTop - this.containerTop);
+			y = this.arrayMoves[i].top;
+
+			// console.log(y);
+			this.arrayMoves[i].el.style[this.transform] = 'translate3d(0, ' + y + 'px, 0)';
+
+		}
 
 	}
 	
